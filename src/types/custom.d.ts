8787d650b92bf4ba08e98d4716237210800a5d1e
Nacho Moya <nacho.moya@strivelabs.io>
2024-09-06 18:13:26 +0200

    .vert and .frag files do not throw TS error anymore

---
+++ src/types/custom.d.ts
@@ -0,0 +1,10 @@
+// src/types/custom.d.ts
+declare module "*.frag" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.vert" {
+  const content: string;
+  export default content;
+}
